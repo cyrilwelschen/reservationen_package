d5c80b3b8a4e5b463c81766f0eda554ae5c90f51
cyrilwelschen <cj.welschen@gmail.com>
2018-07-31 14:05:21 +0200

    patterns and meanings start

--- analyse.py
+++ analyse.py
@@ -2,6 +2,7 @@
 
 import os
 import sqlite3
+from patterns_and_meanings import perform_matching
 
 
 def an_format(anab_str):
@@ -23,7 +24,7 @@ res_id_deleted = []
 
 ##
 row_list = []
-with open("Prot.csv", "rb") as fi:
+with open("/home/cyril/Desktop/GastroDat2/Prot.csv", "rb") as fi:
     for line in fi:
         row_list.append(line.decode("iso-8859-1"))
 
@@ -35,6 +36,9 @@ for row in row_list:
 # process data
 uniq_ana = []
 uniq_ana_f = []
+count = 0
+matched = 0
+no_match = 0
 for row in list_of_row_lists:
     r = []
     for i in row:
@@ -42,33 +46,52 @@ for row in list_of_row_lists:
             r += i.split(";")
         else:
             r.append(i)
+    clean_row = []
+    for i in r:
+        try:
+            clean_row.append(int(i))
+        except ValueError:
+            clean_row.append(i)
 
-    """
-    # check 1: column 5 is in 300, 301, ...
-    if r[3][:len("prot_Aufen")] == "prot_Aufen":
-        if r[3][-len("scht"):] == "scht":
-            split_one = r[3].split(":")[1]
-            split_two = split_one.split(" ")
-            res_id_deleted.append(int(split_two[0]))
-    try:
-        if r[4] in [" 300", " 301", " 302", " 303", " 304", " 304", " 305", " 306", " 307", " 308", " 309", " 310",
-                    " 311", " 312", " 314", " 315", " 316", " 317", " 320", " 330", " 340", " 350"]:
-            # check 2: column 4 starts with Terminxx...
-            if r[3][:len("Termin")] == "Termin":
-                # check 3: if column 7 is "AUFENTHALT" then 8 is ID 
-                if r[6] == "AUFENTHALT":
-                    db_list_of_lists.append(
-                        [r[7], r[4], an_format(r[5]), ab_format(r[5]), name(r[3]), r[0] + " " + r[1]])
-                # check 3.2 else column 9 is "AUFENTHALT" then 10 is ID
-                elif r[8] == "AUFENTHALT":
-                    db_list_of_lists.append(
-                        [r[9], r[4], an_format(r[5]), ab_format(r[5]), name(r[3]), r[0] + " " + r[1]])
-        else:
-            uniq_ana.append(r[3][:10])
-            uniq_ana_f.append(r[3])
-    except IndexError:
-        print(r)
-    """
+    found, list = perform_matching(clean_row)
+    if found:
+        matched += 1
+    else:
+        no_match += 1
+        if count < 3000:
+            print(len(clean_row), clean_row)
+        count += 1
+
+print("matches: ", matched)
+print("no matches: ", no_match)
+
+"""
+# remember which reservaiton IDs were deleted
+if r[3][:len("prot_Aufen")] == "prot_Aufen":
+    if r[3][-len("scht"):] == "scht":
+        split_one = r[3].split(":")[1]
+        split_two = split_one.split(" ")
+        res_id_deleted.append(int(split_two[0]))
+# check 1: column 5 is in 300, 301, ...
+try:
+    if r[4] in [" 300", " 301", " 302", " 303", " 304", " 304", " 305", " 306", " 307", " 308", " 309", " 310",
+                " 311", " 312", " 314", " 315", " 316", " 317", " 320", " 330", " 340", " 350"]:
+        # check 2: column 4 starts with Terminxx...
+        if r[3][:len("Termin")] == "Termin":
+            # check 3: if column 7 is "AUFENTHALT" then 8 is ID 
+            if r[6] == "AUFENTHALT":
+                db_list_of_lists.append(
+                    [r[7], r[4], an_format(r[5]), ab_format(r[5]), name(r[3]), r[0] + " " + r[1]])
+            # check 3.2 else column 9 is "AUFENTHALT" then 10 is ID
+            elif r[8] == "AUFENTHALT":
+                db_list_of_lists.append(
+                    [r[9], r[4], an_format(r[5]), ab_format(r[5]), name(r[3]), r[0] + " " + r[1]])
+    else:
+        uniq_ana.append(r[3][:10])
+        uniq_ana_f.append(r[3])
+except IndexError:
+    print(r)
+"""
 
 for i in set(uniq_ana):
     for j, ele in enumerate(uniq_ana):
--- patterns_and_meanings.py
+++ patterns_and_meanings.py
@@ -1,6 +1,35 @@
-def known_patterns_and_meanings():
+def test_known_patterns_and_meanings():
     pat1 = (['T', 'T', "prot_Aufen", 'i'], ['', '', '', ''])
     return [pat1]
+
+
+def test_patterns():
+    test1 = ['asdf', 3, 'prot_aufen', 3]
+    test2 = ['asdf', 3, 'prot_Aufen', 3]
+    test3 = ['asdf', 3, 'prot_Aufen', 3.7]
+    test4 = ['asdf', 3, 'prot_Aufenthalt', 3]
+    for pat in test_known_patterns_and_meanings():
+        print(process_line(pat, test1))
+        print(process_line(pat, test2))
+        print(process_line(pat, test3))
+        print(process_line(pat, test4))
+
+
+def known_patterns_and_meanings():
+    # pat1 = ([20161110, '16:59:17', '0.00', 'Termin geändert: Welschen', 308, ' 11.11.2016-13.11.2016', 'AUFENTHALT', 12, 7413, 7, 'RECEPTION', 2], [])
+    pat1 = (['i', 'T', 'T', 'Termin ge', 'i', 'T', 'AUFENTHALT', 'i', 'i', 'i', 'RECEPTION', 'i'], ['']*12)
+    # igo1 = ([20161110, '10:41:45', '', 'prot_Zimmerdaten eingestellt', 'ZIMMERDATEN', 3, 0, 21, '', 0])
+    ign1 = (['i', 'T', 'T', 'prot_Zimmerdaten eingestellt', 'ZIMMERDATEN', 'i', 'i', 'i', 'T', 'i'], [False])
+    # ign2 = ([20170129, '18:33:14', '0.00', 'Termineinsicht: Welschen', 309, ' 10.11.2016-11.11.2016', 'AUFENTHALT', 10, 7413, 8, 'RECEPTION', 2])
+    ign2 = (['i', 'T', 'T', 'Termineinsicht', 'i', 'T', 'AUFENTHALT', 'i', 'i', 'i', 'RECEPTION', 'i'], [False])
+    # ign3 = ([20170204, '18:53:20', '0.00', 'Dokument erstellt: Rechnung (D)', 'DOKUMENT', 143, 1793, 0, 'RECEPTION', 2])
+    ign3 = (['i', 'T', 'T', 'Dokument erstellt', 'DOKUMENT', 'i', 'i', 'i', 'RECEPTION', 'i'], [False])
+    # ign4 = ([20170210, '09:36:02', '0.00', 'Terminstatus: CheckOut', 310, ' 03.02.2017-10.02.2017', 'AUFENTHALT', 146, 4187, 9, 'RECEPTION', 2], [False])
+    ign4 = (['i', 'T', 'T', 'Terminstatus: CheckOut', 'i', 'T', 'AUFENTHALT', 'i', 'i', 'i', 'RECEPTION', 'i'], [False])
+    # ign5 = ([20170210, '09:36:21', '0.00', 'Terminstatus: Belegung', 300, ' 10.02.2017-15.02.2017', 'AUFENTHALT', 231, 8076, 15, 'RECEPTION', 2], [False])
+    ign5 = (['i', 'T', 'T', 'Terminstatus: Belegung', 'i', 'T', 'AUFENTHALT', 'i', 'i', 'i', 'RECEPTION', 'i'], [False])
+    # ign6 = (, [False])
+    return [pat1, ign1, ign2, ign3, ign4, ign5]
 
 
 def make_func(p):
@@ -12,38 +41,39 @@ def make_func(p):
         return lambda x: x[:len(p)] == p
 
 
-def test_patterns():
-    test1 = ['asdf', 3, 'prot_aufen', 3]
-    test2 = ['asdf', 3, 'prot_Aufen', 3]
-    test3 = ['asdf', 3, 'prot_Aufen', 3.7]
-    test4 = ['asdf', 3, 'prot_Aufenthalt', 3]
+def perform_matching(item_list):
+    found, li = False, []
     for pat in known_patterns_and_meanings():
-        print(process_line(pat, test1))
-        print(process_line(pat, test2))
-        print(process_line(pat, test3))
-        print(process_line(pat, test4))
+        if len(pat[0]) == len(item_list):
+            f, l = process_line(pat, item_list)
+            if f == True:
+                found = True
+                li = l
+    return found, li
 
 
-def process_line(pattern_and_meaning, test_list):
-    if match_pattern(pattern_and_meaning[0], test_list):
+def process_line(pattern_and_meaning, item_list):
+    if match_pattern_one_by_one(pattern_and_meaning[0], item_list):
         meaning = pattern_and_meaning[1]
-        # todo: handle matches, but ignores
-        return convert_to_meaning(meaning, test_list)
+        if meaning:
+            return True, convert_to_meaning(meaning, item_list)
+        else:
+            return True, []
     else:
         # todo: send mail with no-match
-        # return "no match: ", test_list
-        return []
+        # return "no match: ", item_list
+        return False, []
 
 
-def convert_to_meaning(meaning, test_list):
+def convert_to_meaning(meaning, item_list):
     return_list = []
-    for m, item in zip(meaning, test_list):
+    for m, item in zip(meaning, item_list):
         if m == '':
             return_list.append(item)
     return return_list
 
 
-def match_pattern(func_list, param_list):
+def match_pattern_one_by_one(func_list, param_list):
     match = True
     for f, p in zip(func_list, param_list):
         func = make_func(f)
