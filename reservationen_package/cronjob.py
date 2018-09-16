5cfed7ea346af561e33f966d94778e8cfb03a23d
cyrilwelschen <cj.welschen@gmail.com>
2018-09-16 22:15:01 +0200

    fix cronjob file to not ask for path

--- reservationen_package/cronjob.py
+++ reservationen_package/cronjob.py
@@ -5,5 +5,5 @@ from reservationen_package import single_run_master
 
 
 if __name__ == "__main__":
-    current_path = os.path.dirname(__file__)
-    single_run_master.do_single_run(current_path)
+    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs.txt")
+    single_run_master.do_single_run(config_path)
--- reservationen_package/start_cron.py
+++ reservationen_package/start_cron.py
@@ -6,6 +6,7 @@ from reservationen_package.configs import Configs
 
 if __name__ == "__main__":
     con = Configs("configs.txt")
+    # todo: make this windows compatible with using CronTab(tab=""" ***** commmand"""). See python-crontab doc...
     my_cron = CronTab(user=True)
     print(con.configs["working_dir"])
     command = "python3 {}".format(os.path.join(con.configs["working_dir"], "cronjob.py"))
